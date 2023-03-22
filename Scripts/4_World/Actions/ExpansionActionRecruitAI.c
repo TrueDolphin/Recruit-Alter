9c2690c596e1edfc99aa5c62fdd3b53019401811
James <demonsythe.jm@gmail.com>
2023-03-22 13:59:14 +1100

    Update ExpansionActionRecruitAI.c
    altered function to match live.

--- Scripts/4_World/Actions/ExpansionActionRecruitAI.c
+++ Scripts/4_World/Actions/ExpansionActionRecruitAI.c
@@ -2,28 +2,28 @@ modded class ExpansionActionRecruitAI {
   ref Recruit_Groups m_RecruitSettings;
 
   override bool ActionCondition(PlayerBase player, ActionTarget target, ItemBase item) {
-    eAIBase tAI;
-    if (!Class.CastTo(tAI, target.GetObject()))
-      return false;
+		eAIBase tAI;
+		if (!Class.CastTo(tAI, target.GetObject()))
+			return false;
 
-    if (!tAI.IsAlive() || tAI.IsUnconscious())
-      return false;
+		if (!tAI.IsAlive() || tAI.IsUnconscious())
+			return false;
 
-    if (player.GetGroup() == tAI.GetGroup())
-      return false;
+		if (player.GetGroup() == tAI.GetGroup())
+			return false;
 
-    //! Don't allow to recruit other player's AI
-    if (tAI.GetGroup().GetLeader() && !tAI.GetGroup().GetLeader().IsAI())
-      return false;
+		//! Don't allow to recruit other player's AI
+		if (tAI.GetGroup().GetLeader() && !tAI.GetGroup().GetLeader().IsAI())
+			return false;
 
-    //! Don't allow to recruit passive AI
-    if (tAI.eAI_IsPassive())
-      return false;
+		//! Don't allow to recruit passive or invincible AI
+		if (tAI.eAI_IsPassive() || tAI.GetGroup().GetFaction().IsInvincible())
+			return false;
     //setting disabled
     if (!GetExpansionSettings().GetAI().CanRecruitGuards)
       return false;
     //if enemy, fail
-    if (tAI.PlayerIsEnemy(player, false))
+    if (tAI.PlayerIsEnemy(player))
       return false;
 
     //if match allow
